8fec0363e931c5c4193e1c4243cecdf66a6fbafb
Abhijeet Anand <aabhijeet011@gmail.com>
2021-11-24 18:12:17 +0530

    Level-4 Authentication-salting and hashing using bcrypt

--- check.js
+++ check.js
@@ -11,4 +11,25 @@ console.log(md5('message'));
 // This will print the following :- 78e731027d8fd50ed642340b7c9a63b3
 
 
+/* salting and Hashing */
+
+// password + salt == hash + salt = hash + salt = ... is called as salt Rounds.
+
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+
+bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
+    // Store hash in your password DB.
+});
+
+// Load hash from your password DB.
+bcrypt.compare(myPlaintextPassword, hash, function(err, result) {
+    // result == true
+});
+
+
+
+
 
